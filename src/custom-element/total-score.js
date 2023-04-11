2ae30a46c219fcc4d1bd68a47298168703af290c
anjia <anjia@360.cn>
2023-04-11 22:18:33 +0800

    update total-score

--- src/custom-element/total-score.js
+++ src/custom-element/total-score.js
@@ -1,23 +1,45 @@
 customElements.define('total-score', class extends HTMLElement {
+
+    static MAX = 999999
+
+    static get observedAttributes() {
+        return ['score', 'vs']
+    }
+    static #show(x) {
+        return String(x).padStart(6, '0')
+    }
+
     constructor() {
         super()
 
-        // 接收参数
-        const type = this.getAttribute('type')
+        // 实例数据
+        this.type = this.getAttribute('type') || '2'
+        this.domScore = document.createElement('div')
+        this.dataScore;
+
+        this.domDiff = null
+        this.dataDiff;
+        this.dataVS;
 
         // 构造 shadow DOM
         let shadow = this.attachShadow({ mode: 'open' })
 
+        // css
+        const link = document.createElement('link')
+        link.setAttribute('rel', 'stylesheet')
+        link.setAttribute('href', './custom-element/total-score.css')
+        shadow.appendChild(link)
+
         // html
         const text = document.createTextNode('SCORE')
         shadow.appendChild(text)
-        if (type === '2') {
-            const total = document.createElement('div')
-            total.innerText = '000000'
-            shadow.appendChild(total)
-            const dist = document.createElement('div')
-            dist.innerText = '000000'
-            shadow.appendChild(dist)
+        if (this.type === '2') {
+            this.score = this.getAttribute('score')
+            shadow.appendChild(this.domScore)
+
+            this.domDiff = document.createElement('div')
+            this.vs = this.getAttribute('vs') || this.score
+            shadow.appendChild(this.domDiff)
         } else {
             const max = document.createElement('div')
             max.innerText = '最高分 000000'
@@ -25,6 +47,54 @@ customElements.define('total-score', class extends HTMLElement {
             total.innerText = '当前分 000000'
             shadow.appendChild(max)
             shadow.appendChild(total)
+        }
+    }
+
+    get score() {
+        return this.dataScore
+    }
+    get vs() {
+        return this.dataVS
+    }
+
+    set score(x) {
+        x = parseInt(x) || 0
+        if (x === this.dataScore) return
+        if (x > this.constructor.MAX) {
+            x = this.constructor.MAX
+            this.setAttribute('score', x)
+        }
+        this.dataScore = x
+        this.domScore.innerText = this.constructor.#show(this.dataScore)
+    }
+    set vs(x) {
+        x = parseInt(x) || 0
+        if (this.type !== '2' || x === this.dataVS) return
+        this.dataVS = x
+        this.#updateDiff()
+    }
+
+    #updateDiff() {
+        const dist = this.dataScore - this.dataVS
+        if (dist === this.dataDiff) return
+        if (dist >= 0) {
+            this.domDiff.className = 'diff'
+        } else {
+            this.domDiff.className = 'diff less'
+        }
+        this.dataDiff = dist
+        this.domDiff.innerText = this.constructor.#show(Math.abs(dist))
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case 'score':
+                this.score = newValue
+                this.#updateDiff()
+                break
+            case 'vs':
+                this.vs = newValue
+                break
         }
     }
 })
