9d0643d887e80c082fd32ed714475b874773ab80
anjia <anjia@360.cn>
2023-04-25 16:40:43 +0800

    add 日间/夜间

--- src/v2/index.js
+++ src/v2/index.js
@@ -2,6 +2,8 @@ const tetris = document.getElementById('tetris')
 const people = document.getElementById('people')
 const vs = document.getElementById('options')
 const options = document.querySelectorAll('#options input')
+const daynight = document.getElementById('daynight')
+const root = document.querySelector(':root')
 
 // 对战模式
 document.querySelectorAll('input[name="mode"]').forEach(item => {
@@ -32,4 +34,15 @@ document.querySelectorAll('input[name="games"]').forEach(item => {
     item.addEventListener('click', () => {
         tetris.setAttribute('games', item.value)
     })
+})
+
+// 日间/夜间模式
+daynight.addEventListener('click', (e) => {
+    if (daynight.checked) {
+        root.style.setProperty('--color', '#fff')
+        root.style.setProperty('--bg', '#000')
+    } else {
+        root.style.setProperty('--color', '#000')
+        root.style.setProperty('--bg', '#fff')
+    }
 })
