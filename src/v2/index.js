8ee851c07260894f56ba336be2a0ebccadb29467
anjia <anjia@360.cn>
2023-04-25 16:11:45 +0800

    add 三局两胜/五局三胜

--- src/v2/index.js
+++ src/v2/index.js
@@ -1,21 +1,35 @@
-const mode = document.querySelectorAll('input[name="mode"]')
-const people = document.getElementById('people')
 const tetris = document.getElementById('tetris')
+const people = document.getElementById('people')
+const vs = document.getElementById('options')
+const options = document.querySelectorAll('#options input')
 
 // 对战模式
-for (let item of mode) {
+document.querySelectorAll('input[name="mode"]').forEach(item => {
     item.addEventListener('click', () => {
         if (item.value === 'single') {
-            people.disabled = true
+            vs.className = 'disabled'
+            for (let item of options) {
+                item.disabled = true
+            }
             tetris.setAttribute('people', '1')
         } else {
-            people.disabled = false
+            vs.className = ''
+            for (let item of options) {
+                item.disabled = false
+            }
             tetris.setAttribute('people', people.value)
         }
     })
-}
+})
 
 // 对战人数
 people.addEventListener('change', () => {
     tetris.setAttribute('people', people.value)
+})
+
+// 对战场次
+document.querySelectorAll('input[name="games"]').forEach(item => {
+    item.addEventListener('click', () => {
+        tetris.setAttribute('games', item.value)
+    })
 })
